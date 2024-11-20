995a8f1453d0d6f3bcdc9624381cd967678b7107
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2024-11-19 23:13:30 -0500

    Added some basic files to handle authentication

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { config } from 'dotenv';
+import authRouter from './routes/auth';
 
 config();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Connected to http://localhost:${process.env.PORT}`);
